4c70c74a0970c4dac7c324e2c68e93c86be4eeab
laolang2016 <xiaodaima2016@163.com>
2023-06-07 15:52:21 +0800

    ✨ feat(jx-h5-vue2): 首页顶部搜索框

--- jx/jx-h5-vue2/src/App.vue
+++ jx/jx-h5-vue2/src/App.vue
@@ -9,6 +9,9 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+}
+
+body{
+  background-color: #efefef;
 }
 </style>
--- jx/jx-h5-vue2/src/views/HomeView.vue
+++ jx/jx-h5-vue2/src/views/HomeView.vue
@@ -1,11 +1,16 @@
 <template>
   <div>
-    <van-button type="info">hello, jx</van-button>
+    <van-search v-model="searchValue" placeholder="请输入搜索关键词" disabled shape="round"/>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HomeView'
+  name: 'HomeView',
+  data () {
+    return {
+      searchValue: ''
+    }
+  }
 }
 </script>
